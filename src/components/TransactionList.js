f4ccf618fefb1bc67e09b9c5617268f7fcf85876
divinosdev <vivek99.work@gmail.com>
2020-06-19 20:15:13 +0530

    Added delete add actions

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -5,6 +5,7 @@ import { Transaction } from './Transaction'
 export const TransactionList = () => {
 
     const { transactions } = useContext(GlobalContext);
+
     return (
         <>
             <h3>History</h3>
